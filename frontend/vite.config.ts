ac67e7f472ef32a2f21d75cdba5e2a2b86542e22
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2026-01-18 07:54:41 +0300

    CD

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        includePaths: ['src/scss']
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          includePaths: ['src/scss']
+        }
       }
+    },
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
+      'import.meta.env.VITE_CDN_URL': JSON.stringify(env.VITE_CDN_URL),
     }
-  },
+  }
 })
