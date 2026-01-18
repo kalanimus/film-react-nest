3a792a200555e4c16ac9a99ba22ae944df8d222b
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2026-01-18 07:27:09 +0300

    logging constants in front

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,9 @@
+console.log('ALL ENV:', import.meta.env);
+console.log('VITE_API_URL:', import.meta.env.VITE_API_URL);
+console.log('VITE_CDN_URL:', import.meta.env.VITE_CDN_URL);
+
 export const API_URL = import.meta.env.VITE_API_URL ?? '/api/afisha';
 export const CDN_URL = import.meta.env.VITE_CDN_URL ?? '/content/afisha';
+
+console.log('Final API_URL:', API_URL);
+console.log('Final CDN_URL:', CDN_URL);
