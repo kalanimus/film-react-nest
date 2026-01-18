37466d3efd9d4a3f07ca06753bcd39c009c41757
Nikita Kalanchekaev <nikita.kalan@yandex.ru>
2026-01-18 08:49:37 +0300

    CD

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,4 +1,2 @@
-// export const API_URL = import.meta.env.VITE_API_URL ?? '/api/afisha';
-// export const CDN_URL = import.meta.env.VITE_CDN_URL ?? '/content/afisha';
-export const API_URL = 'http://api.kalanimus.film.nomorepartiessbs.ru/afisha';
-export const CDN_URL = 'http://api.kalanimus.film.nomorepartiessbs.ru/content/afisha';
+export const API_URL = import.meta.env.VITE_API_URL ?? '/api/afisha';
+export const CDN_URL = import.meta.env.VITE_CDN_URL ?? '/content/afisha';
